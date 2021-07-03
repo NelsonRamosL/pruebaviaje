37c33aa36533a928824aca5913f25fe1ba83fece
nNlson rRmos Lopez <jerunel@hotmail.com>
2021-07-03 14:20:56 -0400

    se agregaron comentarios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,13 +2,21 @@ $(document).ready(function () {
 
 console.log("entramos en js");
 
+
+
+// intervalo de carrusel
+
 $('.carousel').carousel({
     interval: 2000
   })
 
 
 
+// Tooltip en los titulos
 
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
 
 
